9babdefabf78d82c4f68aaa28f7940b1b903b3ad
Gabriel Pereira Mendes <gabriel.mendes@montreal.com.br>
2024-10-14 11:40:18 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'two-way-data-biding';
+  minhaProp = true;
+
+  pessoa: any = {
+    status: 1,
+    name: undefined,
+  }
+
+  toggleProp() {
+    this.minhaProp = !this.minhaProp;
+  }
+
+  isTruthy() {
+    // return {};
+    // return [];
+    // return true;
+    // return 1;
+    // return "teste";
+    // return 'teste';
+    // return "0";
+    // return "false";
+    // return this.pessoa.status;
+  }
+
+  isFalsy() {
+    // return false;
+    // return 0;
+    // return "";
+    // return '';
+    // return null;
+    // return undefined;
+    // return -0;
+    // return NaN;
+    // return this.pessoa.name;
+    return this.pessoa.endereco?.rua;
+  }
 }
